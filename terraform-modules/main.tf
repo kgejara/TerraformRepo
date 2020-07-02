4f31915852cac54a113b8619512324d505b4a066
kgejara <kgejara@vrbo.com>
2020-07-02 09:50:42 -0500

    api gateway and lamda added
    api gateway and lamda added

--- terraform-modules/main.tf
+++ terraform-modules/main.tf
@@ -155,3 +155,82 @@ resource "aws_key_pair" "dev_ssh_key" {
   public_key = local_file.public_key_pub.content
 }
 
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE API GATEWAY
+# ---------------------------------------------------------------------------------------------------------------------
+resource "aws_api_gateway_rest_api" "api" {
+  name = "myapi"
+}
+
+resource "aws_api_gateway_deployment" "prod" {
+  depends_on  = ["aws_api_gateway_integration.integration"]
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "prod"
+}
+
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "resource"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.lambda.invoke_arn}"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE API LAMDA FUNTION
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_lambda_function" "lambda" {
+  filename      = "function.zip"
+  function_name = "lambda_placeholder_function"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "lambda_function.my_handler"
+  runtime = "python3.8"
+  source_code_hash = "${filebase64sha256("function.zip")}"
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.lambda.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
